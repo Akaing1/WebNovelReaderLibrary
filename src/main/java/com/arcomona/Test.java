88def1928601664c0bcfb9e2667121d33d29d320
Anthony <anthony.arcomona@gmail.com>
2024-07-19 22:21:03 -0400

    styling

--- src/main/java/com/arcomona/Test.java
+++ src/main/java/com/arcomona/Test.java
@@ -15,38 +15,7 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 
 public static void main(String[] args) throws Exception {
-//    //Blank Document
-//    XWPFDocument document = new XWPFDocument();
-//
-//    //Write the Document in file system
-//    FileOutputStream out = new FileOutputStream("test.docx");
-//
-//    //String s = Files.readString(Path.of("src/main/resources/Novels/AcademysSecondSeat/Chapter_1__Prologue.txt"));
-//    BufferedReader reader;
-//
-//    try {
-//        reader = new BufferedReader(new FileReader("src/main/resources/Novels/MaxLevelNewbie/Chapter_1.txt"));
-//        String line = reader.readLine();
-//
-//        while (line != null) {
-//            XWPFParagraph paragraph = document.createParagraph();
-//            XWPFRun run = paragraph.createRun();
-//            run.setText(line);
-//            line = reader.readLine();
-//
-//            run.addBreak(BreakType.PAGE);
-//        }
-//
-//        reader.close();
-//    } catch (IOException e) {
-//        e.printStackTrace();
-//    }
-//    //System.out.print(s);
-//
-//    document.write(out);
-//    out.close();
-//    System.out.println("test.docx written successfully");
-//
+
 
 }
 
